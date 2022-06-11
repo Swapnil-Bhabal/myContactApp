c329e1e0f7a57fe9e099799e8d8872f41a7fd79c
Swapnil Bhabal <swapnilbhabal787@gmail.com>
2022-06-11 16:58:29 +0530

    Some minor changes are done.

--- frontend/src/components/ContactsList.js
+++ frontend/src/components/ContactsList.js
@@ -2,28 +2,25 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const ContactList = () => {
-    const [contacts, setContacts] = useState();
+    const [contacts, setContacts] = useState([]);
 
     useEffect(() => {
-        const fetchContact = async () => {
+        const fetchContacts = async () => {
             const { data } = await axios.get('/api/contacts');
             setContacts(data); 
         };
-
-        fetchContact();
+        
+        fetchContacts();
     }, []);
-
+    
     return (
         <>
             <h1>All Contacts</h1>
             <ul>
-                {contacts.map((contact, key) => {
-                    return (
-                        <>
-                            <li key={key}>{`${contact.firstName} ${contact.lastName}`}</li>
-                        </>
-                    )
-                })}
+            {contacts.map((contact) => (
+                    <li key={contact._id}>{`${contact.firstName} ${contact.lastName}`}</li>
+                )
+            )}
             </ul>
         </>
     )
