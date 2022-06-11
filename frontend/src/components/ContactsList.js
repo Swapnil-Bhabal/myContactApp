699ec6a99ca864474fea1130c1cbe50868caa6de
Swapnil Bhabal <swapnilbhabal787@gmail.com>
2022-06-11 16:05:30 +0530

    Succesfully rendered the data from backend to frontend using axios.

--- frontend/src/components/ContactsList.js
+++ frontend/src/components/ContactsList.js
@@ -1,6 +1,31 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const ContactList = () => {
+    const [contacts, setContacts] = useState();
+
+    useEffect(() => {
+        const fetchContact = async () => {
+            const { data } = await axios.get('/api/contacts');
+            setContacts(data); 
+        };
+
+        fetchContact();
+    }, []);
+
     return (
-        <h1>Hello World</h1>
+        <>
+            <h1>All Contacts</h1>
+            <ul>
+                {contacts.map((contact, key) => {
+                    return (
+                        <>
+                            <li key={key}>{`${contact.firstName} ${contact.lastName}`}</li>
+                        </>
+                    )
+                })}
+            </ul>
+        </>
     )
 }
 
