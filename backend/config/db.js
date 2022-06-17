d19b0895b4704d32acfdc3932e37ab87bd805873
Swapnil Bhabal <swapnilbhabal787@gmail.com>
2022-06-17 06:24:58 +0530

    mongo atlas is added.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectDb = async () => {
     try {
-        const conn = await mongoose.connect("mongodb://localhost:27017/myContactApp", {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
         });
